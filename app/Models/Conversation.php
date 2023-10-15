85a906c23de29eaccee91e272573b6c582a3b95d
Soburr <adebesinbenzenejr99@gmail.com>
2023-10-15 21:44:49 +0100

    feature/relationships

--- app/Models/Conversation.php
+++ app/Models/Conversation.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class Conversation extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'sender_id',
+        'receiver_id',
+        'last_time_message'
+    ];
+
+    public function messages() {
+        return $this->hasMany(Message::class);
+    }
+
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
 }
--- app/Models/Message.php
+++ app/Models/Message.php
@@ -8,4 +8,21 @@ use Illuminate\Database\Eloquent\Model;
 class Message extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'conversation_id',
+        'sender_id',
+        'receiver_id',
+        'read',
+        'body',
+        'type'
+    ];
+
+    public function conversation() {
+        return $this->belongsTo(Conversation::class);
+    }
+
+    public function user() {
+        return $this->belongsTo(User::class, 'sender_id');
+    }
 }
