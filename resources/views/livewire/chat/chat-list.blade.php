2f39ff9f2ae533d7aff36c47bb66426056734157
Soburr <adebesinbenzenejr99@gmail.com>
2023-10-19 17:03:13 +0100

    feature/chatlist

--- resources/views/livewire/chat/chat-list.blade.php
+++ resources/views/livewire/chat/chat-list.blade.php
@@ -3,9 +3,16 @@
     <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
         <div class="chatlist_header">
 
+            <div class="title">
+                Chat
+            </div>
+            <div class="img_container">
+                <img src="https://picsum.photos/id/14/200/390" alt="">
+            </div>
+
         </div>
         <div class="chatlist_body">
-            
+
         </div>
     </h2>
 </div>
