33e056a5faa61102db90ddd8b157826459855673
Soburr <adebesinbenzenejr99@gmail.com>
2023-10-20 07:49:35 +0100

    feature/chatlist

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -24,10 +24,10 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300&display=swap" rel="stylesheet"> 
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300&display=swap" rel="stylesheet">
 
         {{-- css --}}
-        {{-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous"> --}}
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
 
 
@@ -35,7 +35,7 @@
         <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
         <link rel="stylesheet" href="{{ asset('css/chat.css')}}">
         @vite(['resources/css/app.css', 'resources/js/app.js'])
-      
+
         <style>
            body{
               font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
--- resources/views/livewire/chat/chat-list.blade.php
+++ resources/views/livewire/chat/chat-list.blade.php
@@ -13,6 +13,33 @@
         </div>
         <div class="chatlist_body">
 
+            <div class="chatlist_item">
+                <div class="chatlist_img_container">
+                    <img src="https://picsum.photos/id/190/200/390" alt="">
+                </div>
+
+                <div class="chatlist_info">
+                    <div class="top_row">
+                        <div class="list_username">Newton</div>
+                            <span class="date">2d</span>
+                    </div>
+
+                    <div class="bottom_row">
+                        <div class="message_body text-truncate">
+                            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                            Voluptas voluptates dolore maxime eius quas sed ipsum minima
+                             voluptatem illum incidunt. Exercitationem doloribus quidem r
+                             eiciendis veniam corporis sapiente praesentium quis magni.
+                        </div>
+
+                        <div class="unread_count">
+                            56
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
         </div>
     </h2>
 </div>
