7470e8f77c475731f344b61d2bc9ede1f13a4163
Soburr <adebesinbenzenejr99@gmail.com>
2023-10-22 18:42:40 +0100

    feature/chatbox-header

--- resources/views/livewire/chat/chatbox.blade.php
+++ resources/views/livewire/chat/chatbox.blade.php
@@ -2,19 +2,58 @@
     {{-- If you look to others for fulfillment, you will never truly be fulfilled. --}}
     <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
         <div class="chatbox_header">
-          chat header
+            <div class="return">
+               <i class="bi bi-arrow-left"></i>
+            </div>
+
+            <div class="img_container">
+                <img src="https://picsum.photos/id/146/200/390" alt="">
+            </div>
+
+            <div class="name">
+                Newton
+            </div>
+
+            <div class="info">
+
+                <div class="info_item">
+                    <i class="bi bi-telephone-fill"></i>
+                </div>
+
+                <div class="info_item">
+                    <i class="bi bi-card-image"></i>
+                </div>
+
+                <div class="info_item">
+                    <i class="bi bi-info-circle-fill"></i>
+                </div>
+
+            </div>
+
         </div>
         <div class="chatbox_body">
-Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum recusandae officia saepe unde nam amet eos mollitia, labore aliquam quaerat ducimus enim, fugiat totam? Repellat optio suscipit facilis reiciendis maxime?
-Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique ipsum voluptatum, qui quisquam ullam dolorum sed maiores unde dolores consequatur quam nemo illum voluptatem, facere, inventore accusantium veritatis atque veniam.
-Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore perspiciatis, consequatur, tenetur necessitatibus eaque quos assumenda totam molestiae voluptatibus voluptatem laudantium atque nulla alias! Deleniti a quasi facere similique iusto.
-Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti amet sunt repudiandae praesentium obcaecati aspernatur laborum? Hic animi nemo, labore voluptates, illo sapiente aliquid ipsa architecto odit consequuntur quam corporis?
-Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae sit at corporis molestias dolor quas placeat quaerat corrupti, ipsa odit accusantium laborum harum ad possimus consequuntur sunt impedit sapiente voluptatibus?
-Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis eligendi adipisci deleniti ducimus cumque exercitationem earum iure? Doloremque dignissimos optio earum. Velit dolorum dolores magni, inventore exercitationem veniam quaerat ut.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum recusandae officia saepe unde nam amet eos
+            mollitia, labore aliquam quaerat ducimus enim, fugiat totam? Repellat optio suscipit facilis reiciendis
+            maxime?
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique ipsum voluptatum, qui quisquam ullam
+            dolorum sed maiores unde dolores consequatur quam nemo illum voluptatem, facere, inventore accusantium
+            veritatis atque veniam.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore perspiciatis, consequatur, tenetur
+            necessitatibus eaque quos assumenda totam molestiae voluptatibus voluptatem laudantium atque nulla alias!
+            Deleniti a quasi facere similique iusto.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti amet sunt repudiandae praesentium
+            obcaecati aspernatur laborum? Hic animi nemo, labore voluptates, illo sapiente aliquid ipsa architecto odit
+            consequuntur quam corporis?
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae sit at corporis molestias dolor quas
+            placeat quaerat corrupti, ipsa odit accusantium laborum harum ad possimus consequuntur sunt impedit sapiente
+            voluptatibus?
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis eligendi adipisci deleniti ducimus
+            cumque exercitationem earum iure? Doloremque dignissimos optio earum. Velit dolorum dolores magni, inventore
+            exercitationem veniam quaerat ut.
 
         </div>
         <div class="chatbox_footer">
-          chat footer
+            chat footer
         </div>
     </h2>
 </div>
