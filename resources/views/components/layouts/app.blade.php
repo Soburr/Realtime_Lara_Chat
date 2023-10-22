f001a2e46db4252095270100b1fc9427d5c3f62f
Soburr <adebesinbenzenejr99@gmail.com>
2023-10-22 21:43:16 +0100

    feature/styling-message-body

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -44,7 +44,7 @@
         @livewireStyles
     </head>
     <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
+        <div class="min-h-screen"> {{-- dark:bg-gray-900 --}}
             @include('layouts.navigation')
 
             <!-- Page Heading -->
--- resources/views/livewire/chat/chatbox.blade.php
+++ resources/views/livewire/chat/chatbox.blade.php
@@ -1,9 +1,9 @@
 <div>
     {{-- If you look to others for fulfillment, you will never truly be fulfilled. --}}
-    <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+    <h2 class="font text-xl text-gray-800 dark:text-gray-200 leading-tight">
         <div class="chatbox_header">
             <div class="return">
-               <i class="bi bi-arrow-left"></i>
+                <i class="bi bi-arrow-left"></i>
             </div>
 
             <div class="img_container">
@@ -32,24 +32,45 @@
 
         </div>
         <div class="chatbox_body">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum recusandae officia saepe unde nam amet eos
-            mollitia, labore aliquam quaerat ducimus enim, fugiat totam? Repellat optio suscipit facilis reiciendis
-            maxime?
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique ipsum voluptatum, qui quisquam ullam
-            dolorum sed maiores unde dolores consequatur quam nemo illum voluptatem, facere, inventore accusantium
-            veritatis atque veniam.
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore perspiciatis, consequatur, tenetur
-            necessitatibus eaque quos assumenda totam molestiae voluptatibus voluptatem laudantium atque nulla alias!
-            Deleniti a quasi facere similique iusto.
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti amet sunt repudiandae praesentium
-            obcaecati aspernatur laborum? Hic animi nemo, labore voluptates, illo sapiente aliquid ipsa architecto odit
-            consequuntur quam corporis?
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae sit at corporis molestias dolor quas
-            placeat quaerat corrupti, ipsa odit accusantium laborum harum ad possimus consequuntur sunt impedit sapiente
-            voluptatibus?
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis eligendi adipisci deleniti ducimus
-            cumque exercitationem earum iure? Doloremque dignissimos optio earum. Velit dolorum dolores magni, inventore
-            exercitationem veniam quaerat ut.
+            <div class="msg_body msg_body_receiver">
+
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facere eveniet consectetur illum illo.
+                Repellat numquam sed veniam perferendis dolorem est error exercitationem? Nobis eos illum architecto
+                minima vero quae? Ratione?
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut voluptatum repudiandae soluta quo
+                consectetur corporis delectus, nobis tempore similique aspernatur reprehenderit adipisci itaque qui
+                voluptate illum pariatur nihil, dolorum vel?
+
+                <div class="msg_body_footer">
+                    <div class="date">
+                        5 hours ago
+                    </div>
+
+                    <div class="read">
+                        <i class="bi bi-check"></i>
+                    </div>
+                </div>
+            </div>
+
+            <div class="msg_body msg_body_me">
+
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facere eveniet consectetur illum illo.
+                Repellat numquam sed veniam perferendis dolorem est error exercitationem? Nobis eos illum architecto
+                minima vero quae? Ratione?
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut voluptatum repudiandae soluta quo
+                consectetur corporis delectus, nobis tempore similique aspernatur reprehenderit adipisci itaque qui
+                voluptate illum pariatur nihil, dolorum vel?
+
+                <div class="msg_body_footer">
+                    <div class="date">
+                        5 hours ago
+                    </div>
+
+                    <div class="read">
+                        <i class="bi bi-check"></i>
+                    </div>
+                </div>
+            </div>
 
         </div>
         <div class="chatbox_footer">
