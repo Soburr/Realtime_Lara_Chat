0c15689eac6030f3c25e599fb9970702d955dfe6
Soburr <adebesinbenzenejr99@gmail.com>
2023-10-26 07:45:23 +0100

    feature/conversation

--- app/Livewire/Chat/CreateChat.php
+++ app/Livewire/Chat/CreateChat.php
@@ -2,16 +2,45 @@
 
 namespace App\Livewire\Chat;
 
+use App\Models\Conversation;
+use App\Models\Message;
 use App\Models\User;
 use Livewire\Component;
 
 class CreateChat extends Component
 {
     public $users;
+    public $message = 'Hello, How are You?';
 
     public function checkconversation($receiverId) {
 
-         dd($receiverId);
+        //  dd($receiverId);
+
+         $checkedConversation = Conversation::where('receiver_id', auth()->user()->id)
+                               ->where('sender_id', $receiverId)->orWhere('receiver_id', $receiverId)
+                               ->where('sender_id', auth()->user()->id)->get();
+
+        if (count($checkedConversation)==0) {
+            //  dd('NO Conversation');
+
+             $createdConversation = Conversation::create(['receiver_id'=>$receiverId, 'sender_id' => auth()->user()->id, 'last_time_message'=>0]);
+             //Conversation Created
+
+             $createdMessage = Message::create(['conversation_id'=>$checkedConversation->id, 'sender_id' => auth()->user()->id, 'receiver_id' => $receiverId, 'body'=>$this->message]);
+
+             $createdConversation->last_time_message = $createdMessage->created_at;
+             $createdConversation->save();
+
+             dd('saved');
+        }
+
+        elseif (count($checkedConversation) >=1) {
+             dd('Conversation exists');
+        }
+
+        else {
+
+        }
 
     }
 
--- resources/views/livewire/chat/create-chat.blade.php
+++ resources/views/livewire/chat/create-chat.blade.php
@@ -1,12 +1,13 @@
 <div>
     {{-- The Master doesn't talk, he acts. --}}
-    
-     @foreach ($users as $user)
-        <ul class="list-group w-75 mx-auto mt-3 container-fluid">
-            <li class="list-group-item list-group-item-action" wire:click='checkconversation({{ $user->id }})'>{{ $user->name }}</li>
-        </ul>
-    @endforeach
 
+    <ul class="list-group w-75 mx-auto mt-3 container-fluid">
+     @foreach ($users as $user)
+
+            <li class="list-group-item list-group-item-action" wire:click='checkconversation({{ $user->id }})'>{{ $user->name }}</li>
+
+    @endforeach
+    </ul>
 
 
 
